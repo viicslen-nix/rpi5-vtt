bf0079ec130242363138251d76bf7c88bee69599
Victor R <39545521+viicslen@users.noreply.github.com>
2025-11-13 08:33:22 -0500

    Update flake.lock and system.nix for Foundry VTT and WiFi AP configuration improvements

--- system.nix
+++ system.nix
@@ -8,7 +8,7 @@
     ${pkgs.iproute2}/bin/ip addr show wlan0
     echo ""
     echo "Hostapd Status:"
-    systemctl status hostapd
+    systemctl status manual-hostapd
     echo ""
     echo "Connected Clients:"
     ${pkgs.iw}/bin/iw dev wlan0 station dump
@@ -21,10 +21,22 @@
     echo "Foundry VTT Service Status:"
     systemctl status foundry-vtt
     echo ""
-    echo "Available hostnames for clients:"
-    echo "  - http://vtt.local:30000"
-    echo "  - http://foundry.local:30000"
-    echo "  - http://192.168.4.1:30000"
+    echo "Nginx Reverse Proxy Status:"
+    systemctl status nginx
+    echo ""
+    echo "Available access methods for clients:"
+    echo "  WITHOUT PORT (recommended):"
+    echo "    - http://vtt.local/"
+    echo "    - http://foundry.local/"
+    echo "    - http://192.168.4.1/"
+    echo "  WITH PORT (legacy):"
+    echo "    - http://vtt.local:30000"
+    echo "    - http://foundry.local:30000"
+    echo "    - http://192.168.4.1:30000"
+    echo ""
+    echo "To test DNS resolution from a connected device:"
+    echo "  nslookup vtt.local 192.168.4.1"
+    echo "  nslookup foundry.local 192.168.4.1"
   '';
 in {
   imports = with inputs.nixos-raspberrypi.nixosModules; [
@@ -58,6 +70,29 @@ in {
   # Create WPA password file for hostapd
   environment.etc."hostapd.wpa_psk".text = "vttgaming123";
   
+  # Create simple hostapd configuration
+  environment.etc."hostapd/hostapd.conf".text = ''
+    # Basic hostapd configuration for Raspberry Pi 5
+    interface=wlan0
+    driver=nl80211
+    ssid=VTT-Gaming
+    hw_mode=g
+    channel=7
+    wmm_enabled=0
+    macaddr_acl=0
+    auth_algs=1
+    ignore_broadcast_ssid=0
+    wpa=2
+    wpa_passphrase=vttgaming123
+    wpa_key_mgmt=WPA-PSK
+    rsn_pairwise=CCMP
+  '';
+
+  # Set environment variable for SSH agent auth
+  environment.variables = {
+    SSH_AUTH_SOCK = "/tmp/ssh-agent-socket";
+  };
+  
   networking = {
     hostId = "8821e309";
     hostName = name;
@@ -72,16 +107,21 @@ in {
     # Enable IP forwarding for internet sharing
     nat = {
       enable = true;
-      externalInterface = "eth0";  # Assuming ethernet for internet
+      externalInterface = "end0";  # Raspberry Pi 5 ethernet interface
       internalInterfaces = [ "wlan0" ];
     };
     
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 53 67 68 30000 ];
+      allowedTCPPorts = [ 22 53 67 68 80 30000 ];
       allowedUDPPorts = [ 53 67 68 ];
+      # Allow forwarding for NAT
+      extraCommands = ''
+        iptables -A FORWARD -i wlan0 -o end0 -j ACCEPT
+        iptables -A FORWARD -i end0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+      '';
       interfaces.wlan0 = {
-        allowedTCPPorts = [ 22 53 67 68 30000 ];
+        allowedTCPPorts = [ 22 53 67 68 80 30000 ];
         allowedUDPPorts = [ 53 67 68 ];
       };
     };
@@ -103,14 +143,67 @@ in {
   security.polkit.enable = true;
   nix.settings.trusted-users = [name];
 
+  # Configure PAM for passwordless sudo with SSH key authentication
+  security.pam.services.sudo = {
+    sshAgentAuth = true;
+  };
+
+  # Configure sudo settings  
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        users = [ name ];
+        commands = [
+          {
+            command = "ALL";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+  };
+
   systemd = {
+    # Service dependencies
+    services = {
+      dnsmasq = {
+        after = [ "manual-hostapd.service" ];
+        wants = [ "manual-hostapd.service" ];
+      };
+      
+      # Manual hostapd service
+      manual-hostapd = {
+        description = "Manual hostapd service";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "systemd-networkd.service" ];
+        wants = [ "systemd-networkd.service" ];
+        serviceConfig = {
+          Type = "forking";
+          PIDFile = "/run/hostapd.pid";
+          ExecStartPre = [
+            "${pkgs.kmod}/bin/modprobe brcmfmac"
+            "${pkgs.iproute2}/bin/ip link set dev wlan0 up"
+          ];
+          ExecStart = "${pkgs.hostapd}/bin/hostapd -B -P /run/hostapd.pid /etc/hostapd/hostapd.conf";
+          ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+          KillMode = "mixed";
+          Restart = "on-failure";
+          RestartSec = 2;
+        };
+      };
+      
+      systemd-networkd.stopIfChanged = false;
+      systemd-resolved.stopIfChanged = false;
+    };
+    
     network = {
       networks = {
         # Ethernet interface for internet connectivity
         "99-ethernet-default-dhcp" = {
           matchConfig.Name = "eth0";
           networkConfig = {
-            DHCP = "ipv4";
+            DHCP = lib.mkForce "ipv4";
             MulticastDNS = "yes";
           };
         };
@@ -118,22 +211,17 @@ in {
         # WiFi interface for access point (don't use DHCP)
         "10-wlan0" = {
           matchConfig.Name = "wlan0";
+          addresses = [
+            {
+              Address = "192.168.4.1/24";
+            }
+          ];
           networkConfig = {
-            Address = "192.168.4.1/24";
-            IPForward = "yes";
+            IPv4Forwarding = true;
+            IPv6Forwarding = false;
           };
         };
       };
-    };
-    
-    # This comment was lifted from `srvos`
-    # Do not take down the network for too long when upgrading,
-    # This also prevents failures of services that are restarted instead of stopped.
-    # It will use `systemctl restart` rather than stopping it with `systemctl stop`
-    # followed by a delayed `systemctl start`.
-    services = {
-      systemd-networkd.stopIfChanged = false;
-      systemd-resolved.stopIfChanged = false;
     };
   };
 
@@ -141,22 +229,82 @@ in {
     getty.autologinUser = name;
     openssh.enable = true;
     
-    # WiFi access point
-    hostapd = {
+    # Enable SSH agent forwarding
+    openssh.settings = {
+      AllowAgentForwarding = true;
+    };
+    
+    # Nginx reverse proxy for VTT
+    nginx = {
       enable = true;
-      radios.wlan0 = {
-        band = "2g";
-        channel = 7;
-        networks.wlan0 = {
-          ssid = "VTT-Gaming";
-          authentication = {
-            mode = "wpa2-sha256";
-            wpaPasswordFile = "/etc/hostapd.wpa_psk";
+      virtualHosts = {
+        "vtt.local" = {
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:30000";
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
+          };
+        };
+        "foundry.local" = {
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:30000";
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
+          };
+        };
+        "192.168.4.1" = {
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:30000";
+            proxyWebsockets = true;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
           };
         };
       };
     };
     
+    # WiFi access point (disabled temporarily for manual setup)
+    # hostapd = {
+    #   enable = true;
+    #   radios.wlan0 = {
+    #     band = "2g";
+    #     channel = 7;
+    #     networks.wlan0 = {
+    #       ssid = "VTT-Gaming";
+    #       authentication = {
+    #         mode = "wpa2-sha1";
+    #         wpaPasswordFile = "/etc/hostapd.wpa_psk";
+    #       };
+    #     };
+    #   };
+    # };
+    
+    # Configure systemd-resolved to not conflict with dnsmasq
+    resolved = {
+      enable = true;
+      dnssec = "false";
+      domains = [ "~." ];
+      fallbackDns = [ "8.8.8.8" "8.8.4.4" ];
+      # Disable the stub resolver so dnsmasq can use port 53
+      extraConfig = ''
+        DNSStubListener=no
+      '';
+    };
+
     # DNS and DHCP for access point clients
     dnsmasq = {
       enable = true;
@@ -200,6 +348,9 @@ in {
         
         # Log DHCP requests for debugging
         log-dhcp = true;
+        
+        # Only bind to our interface
+        bind-interfaces = true;
       };
     };
 
