6bbc61773e404d1eb445d2e19b909aac3a616e05
Victor R <39545521+viicslen@users.noreply.github.com>
2025-11-17 13:52:46 -0500

    fix: Replace Firefox with ungoogled-chromium for Wayland kiosk setup

--- modules/display.nix
+++ modules/display.nix
@@ -9,9 +9,9 @@
 
   # Install packages needed for Wayland kiosk
   environment.systemPackages = with pkgs; [
-    firefox       # Use Firefox for kiosk
-    cage          # Wayland kiosk compositor
-    # wlr-randr     # Display configuration for wlroots compositors
+    ungoogled-chromium  # Lightweight Chromium without Google dependencies
+    cage                # Wayland kiosk compositor
+    # wlr-randr         # Display configuration for wlroots compositors
   ];
 
   # Ensure the kiosk user has access to video/input devices
@@ -22,12 +22,25 @@
   systemd.services."autovt@tty1".enable = false;
 
   # Wrapper program launched by services.cage
-  environment.etc."vtt-kiosk-firefox.sh" = {
+  environment.etc."vtt-kiosk-chromium.sh" = {
     text = ''
       #!/usr/bin/env bash
-      # Firefox kiosk launcher (env provided by services.cage.environment)
+      # Chromium kiosk launcher (env provided by services.cage.environment)
       sleep 2
-      exec ${pkgs.firefox}/bin/firefox --kiosk "http://${config.vtt.common.primaryDomain}/"
+      exec ${pkgs.ungoogled-chromium}/bin/chromium \
+        --kiosk \
+        --noerrdialogs \
+        --disable-infobars \
+        --no-first-run \
+        --enable-features=OverlayScrollbar \
+        --enable-accelerated-video-decode \
+        --enable-gpu-rasterization \
+        --enable-zero-copy \
+        --disable-smooth-scrolling \
+        --disable-background-networking \
+        --disable-sync \
+        --disable-translate \
+        "http://${config.vtt.common.primaryDomain}/"
     '';
     mode = "0755";
   };
@@ -36,11 +49,13 @@
   services.cage = {
     enable = true;
     user = config.vtt.common.userName;
-    program = "/etc/vtt-kiosk-firefox.sh";
+    program = "/etc/vtt-kiosk-chromium.sh";
     environment = {
-      MOZ_ENABLE_WAYLAND = "1";
-      GDK_BACKEND = "wayland";
-      QT_QPA_PLATFORM = "wayland";
+      # Enable Wayland support
+      WAYLAND_DISPLAY = "wayland-1";
+      XDG_RUNTIME_DIR = "/run/user/1000";
+      # Chromium Wayland flags
+      NIXOS_OZONE_WL = "1";
     };
   };
 }
