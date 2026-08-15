621c12596e9c50197d46724c816310bb41539158
Victor R <39545521+viicslen@users.noreply.github.com>
2026-08-15 15:52:20 -0400

    Enable mDNS for local VTT hostname

--- modules/wifi-ap.nix
+++ modules/wifi-ap.nix
@@ -112,4 +112,14 @@
       DNSStubListener=no
     '';
   };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+    };
+  };
 }
